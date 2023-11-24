727d91786ff724ec24d822b624fc754c522c1af2
AlexDavisNeuwiem <alexdavisneuwiem@gmail.com>
2023-11-24 12:56:37 -0300

    Lista de tarefas acrescentada

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ from lib.test import test
 
 if __name__ == "__main__":
     test()
+
+"""
+TODO:
+
+1) Os processos dever estar em um ciclo (implementar: lista de processos em constats talvez)
+2) Usar um while e definir condição de parada
+"""
